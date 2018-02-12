fe75a0f95a2e6cfcd5bd9fdc0de7ce1b4479425b
adigon2006 <adigon2006@gmail.com>
2018-02-12 21:35:30 +0100

    Mobile Application

---
+++ www/js/controllers.js
@@ -0,0 +1,8 @@
+angular.module('zerviceapp.controllers', [])
+
+.controller('ServiceProviderCtrl', function($scope, $ionicModal, $timeout) {
+// responsible for fetching of service providers
+
+
+})
+
